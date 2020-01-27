80fef743b9091e92608b5412c7ac2c59bd7a0b04
o0copter0o <60256343+o0copter0o@users.noreply.github.com>
2020-01-27 18:17:47 +0700

    Update auth.service.ts

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -5,5 +5,20 @@ import { Injectable } from '@angular/core';
 })
 export class AuthService {
 
+  private accesskey = 'ACCESS_TOKEN';
+
   constructor() { }
+
+  public login(access_token: string){
+    localStorage.setItem('ACCESS_TOKEN', "access_token");
+  }
+
+  public isLoggedIn(){
+    return localStorage.getItem('ACCESS_TOKEN') !== null;
+
+  }
+
+  public logout(){
+    localStorage.removeItem('ACCESS_TOKEN');
+  }
 }
