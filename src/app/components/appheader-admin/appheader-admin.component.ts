8ffd874540de402ecd8d234865d2c14fabaa98c4
patsakornchamp <56577744+patsakornchamp@users.noreply.github.com>
2020-01-29 10:43:56 +0700

    champ

--- src/app/components/appheader-admin/appheader-admin.component.ts
+++ src/app/components/appheader-admin/appheader-admin.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit ,TemplateRef } from '@angular/core';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+import { Router } from '@angular/router';
+import { AuthService } from '../../auth.service';
 @Component({
   selector: 'app-appheader-admin',
   templateUrl: './appheader-admin.component.html',
   styleUrls: ['./appheader-admin.component.css']
 })
 export class AppheaderAdminComponent implements OnInit {
+  modalRef: BsModalRef;
 
-  constructor() { }
+  constructor(private authService: AuthService,private router: Router,private modalService: BsModalService) { }
 
   ngOnInit() {
   }
-
+  logout(){
+    this.authService.logout();
+    this.router.navigateByUrl('/login');
+  }
 }
