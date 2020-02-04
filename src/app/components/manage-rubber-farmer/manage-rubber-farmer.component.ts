6bdf8f04b4fa54e06176b37738a640db732f4621
patsakornchamp <56577744+patsakornchamp@users.noreply.github.com>
2020-02-04 21:24:53 +0700

    manage champ

--- src/app/components/manage-rubber-farmer/manage-rubber-farmer.component.ts
+++ src/app/components/manage-rubber-farmer/manage-rubber-farmer.component.ts
@@ -8,8 +8,50 @@ import { Component, OnInit } from '@angular/core';
 export class ManageRubberFarmerComponent implements OnInit {
 
   constructor() { }
+  SEARCH: any = [{}, {}, {}];
+  resposne: any = [1,2,3]
+  A: any = [
+    {
+      name: 'สวนA',
+      addressRubberPlantation:'ตรัง',
+      numtree:'1000',
+      latitude: 14.020740,
+      longitude: 99.991194,
+      detail: 'ไม่มี'
+    },
+    {
+      name: 'สวนB',
+      addressRubberPlantation:'ชุมพร',
+      numtree:'1300',
+      latitude: 14.020740,
+      longitude: 99.991194,
+      detail: 'ไม่มี'
+    },
+    {
+      name: 'สวนC',
+      addressRubberPlantation:'นครศรี',
+      numtree:'1500',
+      latitude: 14.020740,
+      longitude: 99.991194,
+      detail: 'ไม่มี'
+    },
+  ]
 
   ngOnInit() {
+    this.test();
+  }
+
+  test(){
+    if (this.resposne.length > 10) {
+      let pushfor = this.resposne.length - 10;
+      for (let j = 0; j < pushfor; j++) {
+        this.SEARCH.push({});
+      }
+    }
+    for (let i = 0; i < this.resposne.length; i++) {
+      this.SEARCH[i] = this.A[i];
+    }
+console.log(this.SEARCH)
   }
 
 }
