12e3baea6ed8fe32108183e5804e3a973ea43dc1
o0copter0o <60256343+o0copter0o@users.noreply.github.com>
2020-02-11 17:19:32 +0700

    f

---
+++ src/app/_services/authentication.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { User } from '../_models';
+
+@Injectable({ providedIn: 'root' })
+export class AuthenticationService {
+    private currentUserSubject: BehaviorSubject<User>;
+    public currentUser: Observable<User>;
+
+    constructor(private http: HttpClient) {
+        this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
+        this.currentUser = this.currentUserSubject.asObservable();
+    }
+
+    PHP_API_SERVER = "http://158.108.207.7/rubberProject/demo/ctrl.php";
+
+    public get currentUserValue(): User {
+        return this.currentUserSubject.value;
+    }
+
+    login(data) {
+        return this.http.post<any>(`${this.PHP_API_SERVER}`, data)
+            .pipe(map(user => {
+                console.log(user);
+                console.log(user.token);
+                if (user && user.token) {
+                    localStorage.setItem('currentUser', JSON.stringify(user));
+                    this.currentUserSubject.next(user);
+                }
+                return user;
+            }));
+    }
+
+    logout() {
+        localStorage.removeItem('currentUser');
+        this.currentUserSubject.next(null);
+    }
+}
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ApiService } from '../../api.sercice';
+import { AuthenticationService } from '../../_services';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-login',
@@ -14,6 +16,7 @@ export class LoginComponent implements OnInit {
   constructor(
     private apiService: ApiService,
     private router: Router,
+    private authenticationService: AuthenticationService
   ) { }
   loginForm: any;
   isSubmitted = false;
@@ -39,29 +42,41 @@ export class LoginComponent implements OnInit {
   }
 
   loginDB(loginForm) {
-    let data = {
+    let dataLogin = {
       mod:"login",
       value:{
         "username": loginForm.username,
         "password": loginForm.password
       }
     };
-    console.log(data);
-    this.apiService.read(data).subscribe((resposne: any) => {
-      console.log(resposne['statusUser']);
-      if(resposne[0]['statusUser'] == 1){
-        this.router.navigate(['/farmer']);
-      }
-      else if (resposne[0]['statusUser'] == 2) {
-        this.router.navigate(['/page-cooperative']);
-      }
-      else if (resposne[0]['statusUser'] == 3) {
-        this.router.navigate(['/page-guset-user']);
-      }
-      else{
-        window.alert("รหัสผ่านไม่ถูกต้อง");
-        this.router.navigateByUrl('/login');
-      }
-    });
+    console.log(dataLogin);
+    this.authenticationService.login(dataLogin)
+            .pipe(first())
+            .subscribe(
+                data => {
+                  console.log(data);
+                    // this.router.navigate(['/farmer']);
+                },
+                error => {
+                  window.alert("รหัสผ่านไม่ถูกต้อง");
+                  console.log(error);
+                });
+
+    // this.apiService.read(dataLogin).subscribe((resposne: any) => {
+    //   console.log(resposne['statusUser']);
+    //   if(resposne[0]['statusUser'] == 1){
+    //     this.router.navigate(['/farmer']);
+    //   }
+    //   else if (resposne[0]['statusUser'] == 2) {
+    //     this.router.navigate(['/page-cooperative']);
+    //   }
+    //   else if (resposne[0]['statusUser'] == 3) {
+    //     this.router.navigate(['/page-guset-user']);
+    //   }
+    //   else{
+    //     window.alert("รหัสผ่านไม่ถูกต้อง");
+    //     this.router.navigateByUrl('/login');
+    //   }
+    // });
   }
 }
