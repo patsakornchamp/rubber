1c4f47f99a9a3a3e0edee84840d1e35eceb09433
o0copter0o <60256343+o0copter0o@users.noreply.github.com>
2020-02-28 21:06:16 +0700

    Update chart.js

--- src/assets/js/chart.js
+++ src/assets/js/chart.js
@@ -14,7 +14,10 @@ $(function() {
     var areaChart = new Chart(areaChartCanvas)
 
     var areaChartData = {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        labels: [
+            'January', 'February', 'March', 'April', 'May', 'June',
+            'July', 'August', 'September', 'October', 'November', 'December'
+        ],
         datasets: [{
                 label: 'Electronics',
                 fillColor: 'rgba(210, 214, 222, 1)',
@@ -23,7 +26,11 @@ $(function() {
                 pointStrokeColor: '#c1c7d1',
                 pointHighlightFill: '#fff',
                 pointHighlightStroke: 'rgba(220,220,220,1)',
-                data: [100, 59, 80, 81, 56, 100, 40]
+                // data: [100, 59, 80, 81, 56, 100, 40]
+                data: [
+                    0.25, 0.59, 0.80, 0.81, 0.56, 1.00,
+                    0.40, 0.9, 0.80, 0.70, 0.60, 0.60
+                ]
             },
             {
                 label: 'Digital Goods',
@@ -33,7 +40,11 @@ $(function() {
                 pointStrokeColor: 'rgba(60,141,188,1)',
                 pointHighlightFill: '#fff',
                 pointHighlightStroke: 'rgba(60,141,188,1)',
-                data: [50, 48, 40, 19, 86, 27, 50]
+                // data: [50, 48, 40, 19, 86, 27, 50]
+                data: [
+                    0.50, 0.48, 0.40, 0.19, 0.86, 0.27,
+                    0.50, 0.4, 0.50, 0.34, 0.35, 0.97
+                ]
             }
         ]
     }
